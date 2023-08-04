9590652a8876689ce7c45c81c569b52d431aa530
Soappaper <vitaly.kosolapov@gmail.com>
2023-08-04 19:37:02 +0300

    second commit

--- news/urls.py
+++ news/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
-from .views import NewsListView, NewDetailView
+from .views import NewsListView, NewDetailView, SearchNewsListView, NewAddCreateView, NewUpdateView, NewDeleteView
 
 
 urlpatterns = [
    path('', NewsListView.as_view()),
    path('<int:pk>', NewDetailView.as_view(), name='new'),
-
+   path('search/', SearchNewsListView.as_view()),
+   path('add/', NewAddCreateView.as_view(), name='new_add'),
+   path('edit/<int:pk>', NewUpdateView.as_view(), name='new_edit'),
+   path('delete/<int:pk>', NewDeleteView.as_view(), name='new_delete'),
+   path('article/create/', NewAddCreateView.as_view(), name='article_create'),
+   path('article/<int:pk>/edit', NewUpdateView.as_view(), name='article_edit'),
+   path('article/<int:pk>/delete', NewDeleteView.as_view(), name='article_delete'),
 ]
--- news/views.py
+++ news/views.py
@@ -1,5 +1,8 @@
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+
 from .models import Post
+from .filters import NewsFilter
+from .forms import NewForm
 
 
 class NewsListView(ListView):
@@ -7,9 +10,48 @@ class NewsListView(ListView):
     ordering = '-data_create'
     template_name = 'news.html'
     context_object_name = 'news'
+    paginate_by = 10
+
+
+class SearchNewsListView(ListView):
+    queryset = Post.objects.filter(types='NEWS')
+    ordering = '-data_create'
+    template_name = 'search.html'
+    context_object_name = 'news'
+    paginate_by = 10
+
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['filter'] = NewsFilter(self.request.GET, queryset=self.get_queryset())
+        return context
 
 
 class NewDetailView(DetailView):
     model = Post
     template_name = 'new.html'
+    context_object_name = 'new'
+
+class NewAddCreateView(CreateView):
+    template_name = 'new_add.html'
+    form_class = NewForm
+
+    def form_valid(self, form):
+        post = form.save(commit=False)
+        post.post_types = 'NEWS'
+        # post.author = ???
+        return super().form_valid(form)
+
+class NewUpdateView(UpdateView):
+    template_name = 'new_edit.html'
+    form_class = NewForm
+
+    def get_object(self, **kwargs):
+        id = self.kwargs.get('pk')
+        return Post.objects.get(pk=id)
+
+class NewDeleteView(DeleteView):
+    template_name = 'new_delete.html'
+    queryset = Post.objects.all()
+    success_url = '/news/'
     context_object_name = 'new'
